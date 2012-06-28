3b44787660c7c56a371141d2435c3a116492e643
Bao <huebt@yahoo.com>
2012-06-28 09:16:46 -0500

    Initial Commit

---
+++ TestGitOnLion/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestGitOnLion
+//
+//  Created by Mac Owner on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestGitOnLion/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  TestGitOnLion
+//
+//  Created by Mac Owner on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
